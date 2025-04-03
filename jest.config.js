5ca2dae9906dcfce0f8a211c00cf4c57f5609c80
rajveer <rajveerkaur3@academic.rrc.ca>
2025-04-03 01:43:41 -0500

    Update: Added jest config and updated package files

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    // transform: {
+    //     '^.+\\.tsx?$': 'ts-jest',
+    //   },
+    //   moduleFileExtensions: ['ts', 'js', 'json'],
+};
+
+// 'present: "ts-jest": use ts-jest present to handle TS files
+//  testEnvironment: "node" - setting env to node
