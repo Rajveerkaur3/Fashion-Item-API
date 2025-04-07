c3c79e9b5408de591d837e78d173782afeedf705
rajveer <rajveerkaur3@academic.rrc.ca>
2025-04-06 22:54:57 -0500

    Update .gitignore and firebaseConfig.ts to include Firebase credentials

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,11 +1,19 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
-import serviceAccount from "../project---fashion-item-api-firebase-adminsdk-fbsvc-40dcb29851.json";
+import * as dotenv from 'dotenv';
 
+// Load environment variables from .env file
+dotenv.config();
+
+// Parse the Firebase credentials from the environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
+
+// Initialize Firebase
 initializeApp({
-	credential: cert(serviceAccount as ServiceAccount),
+  credential: cert(serviceAccount as ServiceAccount),
 });
 
+// Get Firestore instance
 const db: Firestore = getFirestore();
 
 export { db };
